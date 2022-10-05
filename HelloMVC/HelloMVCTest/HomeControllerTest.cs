f4d83f00e2cd08beac86dc0ba8a7e3709bcc5ea3
Ashley Parker <112976803+ashley-anne@users.noreply.github.com>
2022-10-05 14:29:22 -0400

    first commit

---
+++ HelloMVC/HelloMVCTest/HomeControllerTest.cs
@@ -0,0 +1,22 @@
+using System.Web.Mvc;
+using Xunit;
+using HelloMVC.Controllers;
+
+
+
+
+namespace HelloMVC.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_ViewResult()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
