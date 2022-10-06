32d303bcfac58d07ee10f16e5eae0de13dee95d0
Ashley Parker <112976803+ashley-anne@users.noreply.github.com>
2022-10-06 11:07:44 -0400

    added index

--- HelloMVC/HelloMVC/Program.cs
+++ HelloMVC/HelloMVC/Program.cs
@@ -1,62 +1,44 @@
 using Microsoft.Extensions.Configuration;
+using HelloMVC;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-public class Startup
-{
-    public Startup(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
+//app.UseEndpoints(endpoints =>
+//{
+//});
 
-    public IConfiguration Configuration { get; }
+app.UseHttpsRedirection();
 
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddMvc();
-    }
+app.UseStaticFiles();
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
+app.UseRouting();
 
-        app.UseStaticFiles();
+app.UseAuthorization();
 
-        app.UseRouting();
+app.MapRazorPages();
 
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            endpoints.MapRazorPages();
-        });
+app.UseEndpoints(endpoints =>
+endpoints.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"));
 
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapRazorPages();
-
-        app.Run();
-    }
-}
+app.Run();
